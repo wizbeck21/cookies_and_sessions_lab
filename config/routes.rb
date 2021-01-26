f783a3cc7aa3f71e5ec49582b20d9177c73504d9
Wesley <wes.beck21@outlook.com>
2021-01-26 09:08:05 -0800

    commit tree adca24b71e1ce684d7549dd251d4f43c94409819

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+  root 'products#index'
+  post '/' => 'products#add'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
